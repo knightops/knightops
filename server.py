9752475f024aed43c7ba0975052874b14700c585
zhuxixi <zhuxixi@huayun.com>
2020-07-06 22:22:30 +0800

    修改默认启动端口为8089

--- server.py
+++ server.py
@@ -5,4 +5,4 @@ app_name = 'app'
 app = init_application(app_name)
 
 if __name__ == "__main__":
-    uvicorn.run(app="server:app", host='0.0.0.0', port=9091, log_level='info', reload=True, workers=2)
+    uvicorn.run(app="server:app", host='0.0.0.0', port=8089, log_level='info', reload=True, workers=2)
